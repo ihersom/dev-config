3dc887ba9a794319e519a5434b9d11666c9657d0
ihersom <61396410+ihersom@users.noreply.github.com>
2023-12-15 09:52:06 -0700

    added nvim symlink and lazygit install using curl

--- setup_nvim.sh
+++ setup_nvim.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
 
+#Script Assumes you are running from inside nvim-config repo (git clone https://github.com/ihersom/nvim-config.git)
+
+#Install nvim system wide
 cd ~
 wget https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
 chmod u+x nvim.appimage
 ./nvim.appimage --appimage-extract
+ln -s ~/squashfs-root/usr/bin/nvim /usr/bin/nvim
 
-git clone https://github.com/ihersom/nvim-config.git
-mkdir -p ~/.config/nvim/; cp -dr nvim-config/* ~/.config/nvim
+#Copy Config Over
+mkdir -p ~/.config/nvim/
+cp -dr * ~/.config/nvim
+
+#Install lazygit so neovim plugin can work
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
+
 
 # Now startup neovim anywhere using the "nvim" command, and it will start to install plugins defined by the init.lua config
 # 	- you might experience some problems installing plugins using :Mason from within nvim if python3-virtualenv or python3-venv, npm, and nodejs are not installed
 #	- will need to install lazygit to use the :LazyGit nvim plugin - https://github.com/jesseduffield/lazygit#installation
 #   - Ubuntu
       >> sudo apt install libfuse-dev npm nodejs
-#		- RHEL/RockyLinux/CentOS 
-#			>> sudo dnf copr enable atim/lazygit -y
-#			>> sudo dnf install lazygit
+#   - RHEL/RockyLinux/CentOS 
+#     >> sudo dnf copr enable atim/lazygit -y
+#     >> sudo dnf install lazygit
