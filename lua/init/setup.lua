2a6b2ece54c0f3f5fcf2a378a66f8c58ec0026f1
ihersom <61396410+ihersom@users.noreply.github.com>
2024-01-02 21:03:13 -0700

    update setup.lua with the nvim-tree plugin

--- lua/init/setup.lua
+++ lua/init/setup.lua
@@ -210,6 +210,14 @@ require('lazy').setup({
     end
   },
   {
+    'nvim-tree/nvim-tree.lua',
+    branch = 'v0.99',
+    dependencies = {'nvim-tree/nvim-web-devicons'},
+    config = function()
+      require("nvim-tree").setup {}
+    end,
+  },
+  {
     -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     dependencies = {
