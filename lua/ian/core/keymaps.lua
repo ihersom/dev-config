d9ba448f0b3a65275a96c43097c3acaf61b79754
ihersom <ian.hersom@gmail.com>
2024-01-22 19:59:19 -0700

    updated barbar tab keymaps + barbar opts

--- lua/ian/core/keymaps.lua
+++ lua/ian/core/keymaps.lua
@@ -1,7 +1,16 @@
 --Easy mapping
 local nmap = function(keys, func, desc)
-    vim.keymap.set('n', keys, func, desc, {noremap = true})
+    vim.keymap.set(
+        'n',
+        keys,
+        func,
+        {
+            desc = desc,
+            noremap = true
+        }
+    )
 end
+
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
@@ -14,7 +23,9 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   pattern = '*',
 })
 
--- Telescope --
+
+-------------------------------
+--------- Telescope -----------
 vim.api.nvim_set_keymap(
   "n",
   "<space>fb",
@@ -47,16 +58,44 @@ nmap('<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch 
 nmap('<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 nmap('<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
 nmap('<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
+-------------------------------
+-------------------------------
+
 
 -- Toggle Terminal 3rd Party Utils --
 nmap('<leader>lg', require("ian.plugins.util.terminal3rdparty").git_client_toggle, {desc = 'Open LazyGit UI'})
 nmap('<leader>bt', require("ian.plugins.util.terminal3rdparty").system_info_toggle, {desc = 'Open Bottom Monitor UI'})
 nmap('<leader>ld', require("ian.plugins.util.terminal3rdparty").docker_info_toggle, {desc = 'Open LazyDocker UI'})
 
--- NvimTree
-nmap('<leader>r', ':NvimTreeToggle<CR>', {desc = 'Toggle Nvim Tree'})
--- Navigation
-nmap('<C-M-j>', '7j', {desc = ''})
-nmap('<C-M-k>', '7k', {desc = ''})
---Barbar Tabs
 
+-- NvimTree --
+nmap('<leader>r', ':NvimTreeToggle<CR>', 'Toggle Nvim Tree')
+
+
+-- Navigation --
+nmap('<C-M-j>', '7j', 'Quick nav down w Ctrl+Alt+j')
+nmap('<C-M-k>', '7k', 'Quick nav up Ctrl+Alt+j')
+
+
+-- Barbar Tabs --
+    -- Move to previous/next
+nmap('<A-,>', '<Cmd>BufferPrevious<CR>', "Previous Tab")
+nmap('<A-.>', '<Cmd>BufferNext<CR>', "Next tab")
+    -- Re-order to previous/next
+nmap('<A-<>', '<Cmd>BufferMovePrevious<CR>', 'Move Previous Tab')
+nmap('<A->>', '<Cmd>BufferMoveNext<CR>', 'Move Next Tab')
+    -- Goto buffer in position...
+nmap('<A-1>', '<Cmd>BufferGoto 1<CR>', 'Go to tab 1')
+nmap('<A-2>', '<Cmd>BufferGoto 2<CR>', 'Go to tab 2')
+nmap('<A-3>', '<Cmd>BufferGoto 3<CR>', 'Go to tab 3')
+nmap('<A-4>', '<Cmd>BufferGoto 4<CR>', 'Go to tab 4')
+nmap('<A-5>', '<Cmd>BufferGoto 5<CR>', 'Go to tab 5')
+nmap('<A-6>', '<Cmd>BufferGoto 6<CR>', 'Go to tab 6')
+nmap('<A-7>', '<Cmd>BufferGoto 7<CR>', 'Go to tab 7')
+nmap('<A-8>', '<Cmd>BufferGoto 8<CR>', 'Go to tab 8')
+nmap('<A-9>', '<Cmd>BufferGoto 9<CR>', 'Go to tab 9')
+nmap('<A-0>', '<Cmd>BufferLast<CR>', 'Go to last tab')
+    -- Pin/unpin buffer
+nmap('<A-p>', '<Cmd>BufferPin<CR>')
+    -- Close buffer
+nmap('<A-c>', '<Cmd>BufferClose<CR>')
