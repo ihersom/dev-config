57a191b6a760e169d7c8e33603863d4fee4d3539
ihersom <61396410+ihersom@users.noreply.github.com>
2024-01-26 10:53:13 -0700

    update nvim-tree default settings

--- lua/ian/plugins/tree.lua
+++ lua/ian/plugins/tree.lua
@@ -3,6 +3,19 @@ return {
     branch = 'v0.99', --'v0.99',
     dependencies = {'nvim-tree/nvim-web-devicons'},
     config = function()
-      require("nvim-tree").setup {}
+        require("nvim-tree").setup{
+            sort = {
+                sorter = "case_sensitive",
+            },
+            view = {
+                width = 50,
+            },
+            renderer = {
+                group_empty = true,
+            },
+            filters = {
+                dotfiles = true,
+            },
+      }
     end,
 }
