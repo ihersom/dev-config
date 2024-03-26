a65536f0b9f32c752e5f1857a514923f4cb696b4
ihersom <61396410+ihersom@users.noreply.github.com>
2024-03-25 21:42:20 -0600

    add option se autochdir to be on by default

--- lua/ian/core/options.lua
+++ lua/ian/core/options.lua
@@ -36,3 +36,6 @@ vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
 vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
 vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
 vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
+
+-- vim.cmd [[ set autochdir ]]
+vim.opt.autochdir = true
